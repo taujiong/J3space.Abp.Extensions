25122fe462f23bc6f8b098c4a50b4b84794627b0
taujiong <taujiong@outlook.com>
2020-07-24 20:25:05 +0800

    （临时）Identity Server 的登录登出

---
+++ IdentityServer/J3space.Abp.IdentityServer.Web/Pages/IdentityServer/IdentityServerLogin.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Services;
+using IdentityServer4.Stores;
+using J3space.Abp.Account;
+using J3space.Abp.Account.Pages.Account;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.DependencyInjection;
+
+namespace J3space.Abp.IdentityServer.Web.Pages.IdentityServer
+{
+    [ExposeServices(typeof(Login))]
+    public class IdentityServerLogin : Login
+    {
+        private readonly IClientStore _clientStore;
+        private readonly IEventService _identityServerEvents;
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public IdentityServerLogin(
+            IAccountAppService accountAppService,
+            IIdentityServerInteractionService interaction,
+            IClientStore clientStore,
+            IEventService identityServerEvents
+        ) : base(accountAppService)
+        {
+            _interaction = interaction;
+            _clientStore = clientStore;
+            _identityServerEvents = identityServerEvents;
+        }
+
+        public override async Task<IActionResult> OnGetAsync()
+        {
+            LoginInput = new LoginDto();
+
+            var context = await _interaction.GetAuthorizationContextAsync(ReturnUrl);
+
+            if (context != null) LoginInput.UserNameOrEmailAddress = context.LoginHint;
+
+            return Page();
+        }
+
+        public override async Task<IActionResult> OnPostAsync(string action)
+        {
+            await AccountAppService.Logout();
+            return Page();
+        }
+    }
+}
---
+++ IdentityServer/J3space.Abp.IdentityServer.Web/Pages/IdentityServer/IdentityServerLogout.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using IdentityServer4.Services;
+using J3space.Abp.Account;
+using J3space.Abp.Account.Pages.Account;
+using J3space.Abp.Account.Web.Pages.Account;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.DependencyInjection;
+
+namespace J3space.Abp.IdentityServer.Web.Pages.IdentityServer
+{
+    [ExposeServices(typeof(Login))]
+    public class IdentityServerLogout : Logout
+    {
+        private readonly IIdentityServerInteractionService _interaction;
+
+        public IdentityServerLogout(
+            IAccountAppService accountAppService,
+            IIdentityServerInteractionService interaction
+        ) : base(accountAppService)
+        {
+            _interaction = interaction;
+        }
+
+        public override async Task<IActionResult> OnGetAsync()
+        {
+            await AccountAppService.Logout();
+            return Redirect(ReturnUrl);
+        }
+    }
+}
