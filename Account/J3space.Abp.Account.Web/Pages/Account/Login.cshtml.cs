ad09acd6f870a8fae772b509fdcfdbde2d2acc18
taujiong <taujiong@outlook.com>
2020-07-24 12:02:44 +0800

    修改登录页面，实现登录功能，但是 ModelState 不能验证有效

--- Account/J3space.Abp.Account.Web/Pages/Account/Login.cshtml.cs
+++ Account/J3space.Abp.Account.Web/Pages/Account/Login.cshtml.cs
@@ -1,11 +1,101 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Volo.Abp.Auditing;
+using Volo.Abp.Identity;
+using Volo.Abp.Validation;
 
 namespace J3space.Abp.Account.Pages.Account
 {
-    public class Login : PageModel
+    public class Login : AccountPageModel
     {
-        public void OnGet()
+        public Login(Microsoft.AspNetCore.Identity.SignInManager<IdentityUser> signInManager,
+            IdentityUserManager userManager
+        )
         {
+            SignInManager = signInManager;
+            UserManager = userManager;
+        }
+
+        [HiddenInput]
+        [BindProperty(SupportsGet = true)]
+        public string ReturnUrl { get; set; }
+
+        [HiddenInput]
+        [BindProperty(SupportsGet = true)]
+        public string ReturnUrlHash { get; set; }
+
+        [BindProperty] public LoginInputModel LoginInput { get; set; }
+
+        [BindProperty] public RegisterDto RegisterInput { get; set; }
+
+        private IdentityUserManager UserManager { get; }
+        private Microsoft.AspNetCore.Identity.SignInManager<IdentityUser> SignInManager { get; }
+
+        public virtual IActionResult OnGetAsync()
+        {
+            LoginInput = new LoginInputModel();
+            return Page();
+        }
+
+        public virtual async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid) return Page();
+
+            await ReplaceEmailToUsernameOfInputIfNeeds();
+
+            var result = await SignInManager.PasswordSignInAsync(
+                LoginInput.UserNameOrEmailAddress,
+                LoginInput.Password,
+                LoginInput.RememberMe,
+                true
+            );
+
+            if (result.IsLockedOut) return Page();
+
+            if (result.IsNotAllowed) return Page();
+
+            if (!result.Succeeded) return Page();
+
+            //TODO: Find a way of getting user's id from the logged in user and do not query it again like that!
+            var user = await UserManager.FindByNameAsync(LoginInput.UserNameOrEmailAddress) ??
+                       await UserManager.FindByEmailAsync(LoginInput.UserNameOrEmailAddress);
+
+            Debug.Assert(user != null, nameof(user) + " != null");
+
+            return RedirectSafely(ReturnUrl, ReturnUrlHash);
+        }
+
+        protected virtual async Task ReplaceEmailToUsernameOfInputIfNeeds()
+        {
+            if (!ValidationHelper.IsValidEmailAddress(LoginInput.UserNameOrEmailAddress)) return;
+
+            var userByUsername =
+                await UserManager.FindByNameAsync(LoginInput.UserNameOrEmailAddress);
+            if (userByUsername != null) return;
+
+            var userByEmail = await UserManager.FindByEmailAsync(LoginInput.UserNameOrEmailAddress);
+            if (userByEmail == null) return;
+
+            LoginInput.UserNameOrEmailAddress = userByEmail.UserName;
+        }
+
+        public class LoginInputModel
+        {
+            [Required]
+            [DynamicStringLength(typeof(IdentityUserConsts),
+                nameof(IdentityUserConsts.MaxEmailLength))]
+            public string UserNameOrEmailAddress { get; set; }
+
+            [Required]
+            [DynamicStringLength(typeof(IdentityUserConsts),
+                nameof(IdentityUserConsts.MaxPasswordLength))]
+            [DataType(DataType.Password)]
+            [DisableAuditing]
+            public string Password { get; set; }
+
+            public bool RememberMe { get; set; }
         }
     }
 }
