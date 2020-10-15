5a4348c4bd1e28bca044f71c0a50701418491e99
taujiong <taujiong@outlook.com>
2020-10-15 13:08:46 +0800

    refactor(IdentityServer): 重构 IdentityServer ClientAppService
    1. 重构增删改查逻辑
    2. 扩充 Dto 成员

---
+++ modules/IdentityServer/src/J3space.Abp.IdentityServer.Application.Contracts/J3space/Abp/IdentityServer/Clients/Dto/ClientUpdateDto.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace J3space.Abp.IdentityServer.Clients.Dto
+{
+    public class ClientUpdateDto
+    {
+        public virtual string ClientId { get; set; }
+        public virtual string ClientName { get; set; }
+        public virtual string Description { get; set; }
+        public virtual bool Enabled { get; set; }
+        public virtual string ClientUri { get; set; }
+        public virtual string LogoUri { get; set; }
+        public virtual int? ConsentLifetime { get; set; }
+        public virtual int AuthorizationCodeLifetime { get; set; }
+        public virtual int AccessTokenLifetime { get; set; }
+        public virtual int IdentityTokenLifetime { get; set; }
+        public virtual bool AllowOfflineAccess { get; set; }
+        public virtual bool BackChannelLogoutSessionRequired { get; set; }
+        public virtual string BackChannelLogoutUri { get; set; }
+        public virtual bool FrontChannelLogoutSessionRequired { get; set; }
+        public virtual string FrontChannelLogoutUri { get; set; }
+        public virtual bool AllowAccessTokensViaBrowser { get; set; }
+        public virtual bool AllowPlainTextPkce { get; set; }
+        public virtual bool RequirePkce { get; set; }
+        public virtual bool AlwaysIncludeUserClaimsInIdToken { get; set; }
+        public virtual bool AllowRememberConsent { get; set; }
+        public virtual bool RequireConsent { get; set; }
+        public virtual bool RequireClientSecret { get; set; }
+        public virtual int AbsoluteRefreshTokenLifetime { get; set; }
+        public virtual int SlidingRefreshTokenLifetime { get; set; }
+        public virtual int RefreshTokenUsage { get; set; }
+        public virtual bool UpdateAccessTokenClaimsOnRefresh { get; set; }
+        public virtual int DeviceCodeLifetime { get; set; }
+        public virtual string UserCodeType { get; set; }
+        public virtual int? UserSsoLifetime { get; set; }
+        public virtual string PairWiseSubjectSalt { get; set; }
+        public virtual string ClientClaimsPrefix { get; set; }
+        public virtual bool AlwaysSendClientClaims { get; set; }
+        public virtual bool IncludeJwtId { get; set; }
+        public virtual bool EnableLocalLogin { get; set; }
+        public virtual int AccessTokenType { get; set; }
+        public virtual int RefreshTokenExpiration { get; set; }
+        public virtual string ProtocolType { get; set; }
+        public virtual List<ClientClaimDto> Claims { get; set; }
+        public virtual List<string> AllowedScopes { get; set; }
+        public virtual List<string> AllowedGrantTypes { get; set; }
+        public virtual List<string> RedirectUris { get; set; }
+        public virtual List<string> PostLogoutRedirectUris { get; set; }
+        public virtual List<string> AllowedCorsOrigins { get; set; }
+        public virtual List<string> IdentityProviderRestrictions { get; set; }
+        public virtual List<ClientSecretDto> ClientSecrets { get; set; }
+        public virtual List<ClientPropertyDto> Properties { get; set; }
+    }
+}
---
+++ modules/IdentityServer/src/J3space.Abp.IdentityServer.Application/J3space/Abp/IdentityServer/AbpIdentityServerClientExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using IdentityServer4;
+using J3space.Abp.IdentityServer.Clients.Dto;
+using Volo.Abp.IdentityServer.Clients;
+
+namespace J3space.Abp.IdentityServer
+{
+    public static class AbpIdentityServerClientExtensions
+    {
+        public static void SetDefaultValues(this Client client, ClientType clientType)
+        {
+            switch (clientType)
+            {
+                case ClientType.Empty:
+                    break;
+                case ClientType.Device:
+                    client.AddGrantType(IdentityServer4.Models.GrantType.DeviceFlow);
+                    client.AddScope(IdentityServerConstants.StandardScopes.OpenId);
+                    client.RequireClientSecret = false;
+                    client.AllowOfflineAccess = true;
+                    break;
+                case ClientType.WebServer:
+                    client.AddGrantType(IdentityServer4.Models.GrantType.AuthorizationCode);
+                    client.AllowAccessTokensViaBrowser = false;
+                    client.RequireClientSecret = true;
+                    client.RequirePkce = true;
+                    client.AddScope(IdentityServerConstants.StandardScopes.OpenId);
+                    client.AddScope(IdentityServerConstants.StandardScopes.Profile);
+                    ConfigureClientDefaultUrls(client);
+                    break;
+                case ClientType.Spa:
+                    client.AddGrantType(IdentityServer4.Models.GrantType.AuthorizationCode);
+                    client.RequireClientSecret = false;
+                    client.RequirePkce = true;
+                    client.AddScope(IdentityServerConstants.StandardScopes.OpenId);
+                    client.AddScope(IdentityServerConstants.StandardScopes.Profile);
+                    ConfigureClientDefaultUrls(client);
+                    break;
+                case ClientType.WebHybrid:
+                    client.AddGrantType(IdentityServer4.Models.GrantType.Hybrid);
+                    client.AddScope(IdentityServerConstants.StandardScopes.OpenId);
+                    client.AddScope(IdentityServerConstants.StandardScopes.Profile);
+                    ConfigureClientDefaultUrls(client);
+                    break;
+                case ClientType.Native:
+                    client.AddGrantType(IdentityServer4.Models.GrantType.AuthorizationCode);
+                    client.RequireClientSecret = false;
+                    client.AddScope(IdentityServerConstants.StandardScopes.OpenId);
+                    client.AddScope(IdentityServerConstants.StandardScopes.Profile);
+                    break;
+                case ClientType.Machine:
+                    client.AddGrantType(IdentityServer4.Models.GrantType.ClientCredentials);
+                    client.AddScope(IdentityServerConstants.StandardScopes.OpenId);
+                    client.RequireConsent = false;
+                    client.RequireClientSecret = true;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(ClientType));
+            }
+
+            void ConfigureClientDefaultUrls(Client myClient)
+            {
+                if (string.IsNullOrEmpty(myClient.ClientUri)) return;
+                myClient.AddCorsOrigin(myClient.ClientUri);
+            }
+        }
+    }
+}
