608c1eb9ed1529e5c7e8194e885e3c8fb5a207e7
taujiong <taujiong@outlook.com>
2020-10-16 21:38:20 +0800

    feat(Setting): 添加单元测试 base 项目

---
+++ modules/SettingManagement/test/J3space.Abp.SettingManagement.TestBase/AbpSettingManagementTestDataBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System.Threading.Tasks;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Guids;
+using Volo.Abp.SettingManagement;
+using Volo.Abp.Settings;
+
+namespace J3space.Abp.SettingManagement.TestBase
+{
+    public class AbpSettingManagementTestDataBuilder : ITransientDependency
+    {
+        private readonly IGuidGenerator _guidGenerator;
+        private readonly ISettingRepository _settingRepository;
+
+        public AbpSettingManagementTestDataBuilder(
+            ISettingRepository settingRepository,
+            IGuidGenerator guidGenerator)
+        {
+            _settingRepository = settingRepository;
+            _guidGenerator = guidGenerator;
+        }
+
+        public async Task BuildAsync()
+        {
+            await AddSettingsAsync();
+        }
+
+        private async Task AddSettingsAsync()
+        {
+            await _settingRepository.InsertAsync(
+                new Setting(
+                    _guidGenerator.Create(),
+                    "SettingWithProperty",
+                    "prop",
+                    GlobalSettingValueProvider.ProviderName
+                )
+            );
+
+            await _settingRepository.InsertAsync(
+                new Setting(
+                    _guidGenerator.Create(),
+                    "SettingWithDefaultValue",
+                    "default",
+                    GlobalSettingValueProvider.ProviderName
+                )
+            );
+
+            await _settingRepository.InsertAsync(
+                new Setting(
+                    _guidGenerator.Create(),
+                    "SettingWithoutDefaultValue",
+                    "no-default",
+                    GlobalSettingValueProvider.ProviderName
+                )
+            );
+        }
+    }
+}
---
+++ modules/SettingManagement/test/J3space.Abp.SettingManagement.TestBase/TestSettingDefinitionProvider.cs
@@ -0,0 +1,16 @@
+﻿using Volo.Abp.Settings;
+
+namespace J3space.Abp.SettingManagement.TestBase
+{
+    public class TestSettingDefinitionProvider : SettingDefinitionProvider
+    {
+        public override void Define(ISettingDefinitionContext context)
+        {
+            context.Add(new SettingDefinition("SettingWithoutDefaultValue"));
+            context.Add(new SettingDefinition("SettingWithDefaultValue", "123"));
+            context.Add(new SettingDefinition("SettingWithProperty", defaultValue: "default")
+                .WithProperty("MainGroup", "main")
+                .WithProperty("SubGroup", "sub"));
+        }
+    }
+}
