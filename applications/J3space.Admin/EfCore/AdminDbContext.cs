4fcaac4ef1cfba3ad4f5d9d1424be642ea9dfbcf
taujiong <taujiong@outlook.com>
2020-11-15 16:56:55 +0800

    feat(J3Admin): 添加新的 J3Admin 的基本框架

---
+++ applications/J3space.Admin/EfCore/AdminDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Volo.Abp.AuditLogging.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+using Volo.Abp.FeatureManagement.EntityFrameworkCore;
+using Volo.Abp.PermissionManagement.EntityFrameworkCore;
+using Volo.Abp.SettingManagement.EntityFrameworkCore;
+
+namespace J3space.Admin.EfCore
+{
+    public class AdminDbContext : AbpDbContext<AdminDbContext>
+    {
+        public AdminDbContext(DbContextOptions<AdminDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ConfigureAuditLogging();
+            modelBuilder.ConfigureFeatureManagement();
+            modelBuilder.ConfigurePermissionManagement();
+            modelBuilder.ConfigureSettingManagement();
+        }
+    }
+}
---
+++ applications/J3space.Admin/EfCore/AdminDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace J3space.Admin.EfCore
+{
+    public class AdminDbContextFactory : IDesignTimeDbContextFactory<AdminDbContext>
+    {
+        public AdminDbContext CreateDbContext(string[] args)
+        {
+            var configuration = BuildConfiguration();
+
+            var builder = new DbContextOptionsBuilder<AdminDbContext>()
+                .UseMySql(configuration.GetConnectionString("Default"));
+
+            return new AdminDbContext(builder.Options);
+        }
+
+        private static IConfigurationRoot BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            return builder.Build();
+        }
+    }
+}
