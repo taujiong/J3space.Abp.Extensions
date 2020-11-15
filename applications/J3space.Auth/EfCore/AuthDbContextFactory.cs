b5401f242dd1b956284e10a6684a757f50e44c8d
taujiong <taujiong@outlook.com>
2020-11-15 16:55:14 +0800

    feat(J3Auth): 添加新的 J3Auth

---
+++ applications/J3space.Auth/EfCore/AuthDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace J3space.Auth.EfCore
+{
+    public class AuthDbContextFactory : IDesignTimeDbContextFactory<AuthDbContext>
+    {
+        public AuthDbContext CreateDbContext(string[] args)
+        {
+            var configuration = BuildConfiguration();
+
+            var builder = new DbContextOptionsBuilder<AuthDbContext>()
+                .UseMySql(configuration.GetConnectionString("Default"));
+
+            return new AuthDbContext(builder.Options);
+        }
+
+        private static IConfigurationRoot BuildConfiguration()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false);
+
+            return builder.Build();
+        }
+    }
+}
