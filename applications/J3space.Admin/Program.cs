d6700f5a8bf028db8d023a48168b490a8d16705a
taujiong <taujiong@outlook.com>
2020-11-22 14:48:41 +0800

    feat(J3Admin): 填充相关服务

--- applications/J3space.Admin/Program.cs
+++ applications/J3space.Admin/Program.cs
@@ -1,17 +1,49 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
 
 namespace J3space.Admin
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            Log.Logger = new LoggerConfiguration()
+#if DEBUG
+                .MinimumLevel.Debug()
+#else
+                .MinimumLevel.Information()
+#endif
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft.EntityFrameworkCore", LogEventLevel.Warning)
+                .Enrich.WithProperty("Application", "J3Admin")
+                .Enrich.FromLogContext()
+                .WriteTo.Async(c => c.Console())
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting J3Admin.");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "J3Admin terminated unexpectedly!");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         private static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
+                .UseAutofac()
+                .UseSerilog();
     }
 }
